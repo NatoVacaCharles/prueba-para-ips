33bb65293e5953517ef8b69d1941976a2e55e4c2
Renato <rponcel@unsa.edu.pe>
2026-07-02 23:25:56 -0500

    fix: limpieza final de linter con deshabilitación genérica nuevamente

--- website/components/GiscusComponent.tsx
+++ website/components/GiscusComponent.tsx
@@ -8,7 +8,7 @@ export default function GiscusComponent() {
   const { theme, systemTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
 
-  // eslint-disable-next-line
+  /// eslint-disable-next-line react-hooks/set-state-in-effect
   useEffect(() => {
     setMounted(true);
   }, []);
